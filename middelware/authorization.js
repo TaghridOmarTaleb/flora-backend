9dbad575766912e42c7d376c9c9e1d6ccd39a230
taghridtaleb1@gmail.com <taghridomartaleb@gmail.com>
2023-06-06 08:33:52 +0300

    render

--- middelware/authorization.js
+++ middelware/authorization.js
@@ -1,17 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export function authorization(req, res, next) {
-  // const token = req.header("x-auth-token");
-  // if (!token) return res.status(401).send("Access denied. No token provided!");
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).send("Access denied. No token provided!");
 
-  // try {
-  //   const decoded = jwt.verify(token, process.env.jwtPrivateKey);
-  //   req.user = decoded;
-  //   next();
-  // } catch (exception) {
-  //   res.status(400).send("Invalid token!");
-  // }
-  next();
+  try {
+    const decoded = jwt.verify(token, process.env.jwtPrivateKey);
+    req.user = decoded;
+    next();
+  } catch (exception) {
+    res.status(400).send("Invalid token!");
+  }
+  // next();
 }
 
 export default authorization;
